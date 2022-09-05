92dcf781b46d1be9c974f0363b6941e9c9f29e1c
danielpdaniel <artofjquinn@gmail.com>
2022-09-05 13:22:19 -0400

    commit tree 9da4f53d22db0429cb75bca44416c029ee7fd461

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let seenNums = {};
+  for(const number of array){
+    let complement = target - number;
+    if (seenNums[complement]){
+      return true
+    }
+    seenNums[number] = true;
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  ok so noww its only looping through once and comparing on each
+  loop through to an object it's updating so O(n) ?
 */
 
 /* 
@@ -12,6 +23,13 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  -to save on time, instead of nested loops, we're going to loop
+  through the array once, each time figuring out the number
+  needed to add to the current integer to reach the target and 
+  then add the seen numbers to a data structure outside the loop
+  like an object or a set. then on the next loop through, we
+  will compare the current complement to the list of seen numbers
+  to see if there are any matches, returning true if there are.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +47,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([10, 7, -3], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4))
 }
 
 module.exports = hasTargetSum;
